f7149c5f6de479f83253ae0a316cc2b10906b24d
Jonathan Dawson <jonny_dv8@hotmail.com>
2023-06-15 16:06:05 +1200

    working happy-path tests

--- Backend/cypress/e2e/api-tests.cy.js
+++ Backend/cypress/e2e/api-tests.cy.js
@@ -1,3 +1,5 @@
+var toDoID = ''
+
 context("API endpoint health check", () => {
   it("verifies the API is live", () => {
     cy.request("GET", "/api/todoItems").then((response) => {
@@ -11,7 +13,26 @@ context("POST /api/todoItems", () => {
     cy.request("POST", "/api/todoItems", { description: 'item1' }).then((response) => {
       expect(response.status).to.eq(201)
       expect(response.body).to.be.a('string')
-      const toDoID = response.body
+      toDoID = response.body
+    })
+  })
+})
+
+context("GET recently created toDo item", () => {
+  it("returns the recently created toDo item", () => {
+    cy.request("GET", "/api/todoItems/" + toDoID).then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body.id).to.eq(toDoID)
+      expect(response.body.description).to.eq("item1")
+      expect(response.body.isCompleted).to.eq(false)
+    })
+  })
+})
+
+context("PUT /api/todoItems/{id}", () => {
+  it("marks the recently created toDo item as completed", () => {
+    cy.request("PUT", "/api/todoItems/" + toDoID,{ "id": toDoID, "description": "item1", "isCompleted": true }).then((response) => {
+      expect(response.status).to.eq(204)
     })
   })
 })
